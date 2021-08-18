7dcc58351a974d1b81a6cf3145dfc9a89ced7e86
salhindi <calisaraalhindi@gmail.com>
2021-08-18 14:34:04 -0700

    associations

--- db/migrate/20210818194646_create_travel_plans.rb
+++ db/migrate/20210818194646_create_travel_plans.rb
@@ -2,7 +2,7 @@ class CreateTravelPlans < ActiveRecord::Migration[6.1]
   def change
     create_table :travel_plans do |t|
       t.string :name
-      t.date :start_data
+      t.date :start_date
       t.date :end_date
       t.integer :destination_id
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-paris = Destination.create(city: "Paris", country: "France", description: "warm", image: "https://upload.travelawaits.com/ta/uploads/2021/04/eiffel-tower-800x800.jpg"
-new_york = Destination.create(city: "New York City", country: "USA", description: "cold", image: "n/a"
-machu_picchu = Destination.create(city: "Machu Picchu", country: "Peru", description: "warm", image: "n/a"
+paris = Destination.create(city: "Paris", country: "France", description: "warm")
+new_york = Destination.create(city: "New York City", country: "USA", description: "cold")
+machu_picchu = Destination.create(city: "Machu Picchu", country: "Peru", description: "warm")
     
 family_summer = TravelPlan.create(name: "FamilySummer", start_date: 2021-12-12, end_date: 2022-01-01, destination_id: paris.id)
